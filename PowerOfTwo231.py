a25dfd9cde448eeef286d60f3de1035bdd3e3a12
aliroohan <aliroohan321@gmail.com>
2024-03-17 05:53:33 +0500

    Added Power of Two problrm solution(without loop)

--- PowerOfTwo231.py
+++ PowerOfTwo231.py
@@ -1,3 +1,4 @@
+import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n <= 0:
@@ -10,6 +11,16 @@ class Solution:
 
         return True
 
+    def isPowerOfTwoAlternate(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if math.log2(n) % 1 == 0:
+            return True
+        else:
+            return False
+
 
 print(Solution().isPowerOfTwo(16))
 print(Solution().isPowerOfTwo(218))
+print(Solution().isPowerOfTwoAlternate(16))
+print(Solution().isPowerOfTwoAlternate(218))
---
+++ helllo.py
@@ -0,0 +1,4 @@
+import math
+
+a = math.log2(536870912)
+print(a)
