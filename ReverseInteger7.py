1cf0285747a5989188b804d47963074df7fa3dda
aliroohan <aliroohan321@gmail.com>
2024-03-18 05:49:03 +0500

    Added Reverse Integer problrm solution

---
+++ ReverseInteger7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        neg = False
+        if x < 0:
+            x *= -1
+            neg = True
+        elif x == 0:
+            return 0
+
+        num = str(x)
+        y = list(num.strip())
+        if len(y) > 1:
+            for i in range(len(y) // 2):
+                temp = y[i]
+                y[i] = y[len(y) - i - 1]
+                y[len(y) - i - 1] = temp
+
+        num = "".join(y)
+        if int(num) >= 2147483647:
+            return 0
+        if neg:
+            return int(num) * -1
+        else:
+            return int(num)
+
+
+print(Solution().reverse(123))
+print(Solution().reverse(-123))
+print(Solution().reverse(120))
+print(Solution().reverse(1534236469))
