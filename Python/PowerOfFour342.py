a4b077b74407e34d898e210d634be477bfd96456
aliroohan <aliroohan321@gmail.com>
2024-03-28 21:34:19 +0500

    Added Power of four Solution

---
+++ Python/PowerOfFour342.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 0:
+            return False
+        while n != 1:
+            if n % 4 != 0:
+                return False
+            n = n // 4
+
+        return True
+
+
+print(Solution().isPowerOfFour(16))
+print(Solution().isPowerOfFour(5))
