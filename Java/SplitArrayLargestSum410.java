dc7c1c30451851f3588f8e5ab14fdd5b70a5bd05
aliroohan <aliroohan321@gmail.com>
2024-04-03 22:09:19 +0500

    Added Split Array Largest Sum Problem Solution

---
+++ Java/SplitArrayLargestSum410.java
@@ -0,0 +1,36 @@
+public class SplitArrayLargestSum410 {
+    public static void main(String[] args) {
+        int[] nums = {7,2,5,10,8};
+        int k = 2;
+        System.out.println(splitArray(nums, k));
+    }
+    public static int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0;i < nums.length; i++){
+            start = Math.max(start, nums[i]);
+            end += nums[i];
+        }
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            int sum = 0;
+            int pieces = 1;
+            for (int i: nums){
+                if (sum + i > mid){
+                    sum = i;
+                    pieces++;
+                } else{
+                    sum += i;
+                }
+            }
+            if (pieces > k){
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+
+        return end;
+    }
+}
